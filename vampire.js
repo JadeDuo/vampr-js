a1a9a869395146cbccacd1b601ff9a2277b95a46
Jade <jade.duong@telus.com>
2022-12-02 20:09:29 -0800

    adding completed vampire file in new repo, original repo only had access to main branch

--- vampire.js
+++ vampire.js
@@ -10,24 +10,41 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let distance = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      distance += 1;
+      currentVamp = currentVamp.creator;
+    }
+    return distance;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
+  //helper function for common ancestors
+  allAncestors() {
+    const anscestors = [];
+    let vamp = this;
+    while (vamp.creator) {
+      anscestors.push(vamp);
+      vamp = vamp.creator;
+    }
+    return anscestors;
+  }
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
@@ -36,9 +53,25 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let junior;
+    let senior;
+    if (this.isMoreSeniorThan(vampire)) {
+      junior = vampire.allAncestors();
+      senior = this.allAncestors();
+    } else {
+      junior = this.allAncestors();
+      senior = vampire.allAncestors();
+    }
+    for (let vamp of senior) {
+      for (let vampJR of junior) {
+        if (vampJR === vamp) {
+          return vamp;
+        }
+      }
+      
+    }
   }
 }
 
-module.exports = Vampire;
 
+module.exports = Vampire;
